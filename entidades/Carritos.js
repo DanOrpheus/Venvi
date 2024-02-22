e5ff70662b332709bc17f9b995a5ae60c5386d28
aers1105 <angelrs-2000@hotmail.com>
2024-02-22 12:07:27 -0700

    Add: Carrio, ProtuctoCarrito and DireccionDeEnvio entiies

---
+++ entidades/Carritos.js
@@ -0,0 +1,7 @@
+class Carrito{
+    constructor(idUsuario, total) {
+        this.idUsuario=idUsuario
+        this.total=total
+    }
+}
+module.exports = Carrito;
---
+++ entidades/ProductoCarrito.js
@@ -0,0 +1,8 @@
+class ProductoCarrito{
+    constructor(idCarrito,idProducto,cantidad) {
+        this.idCarrito=idCarrito
+        this.idProducto=idProducto
+        this.cantidad=cantidad
+    }
+}
+module.exports = ProductoCarrito;
